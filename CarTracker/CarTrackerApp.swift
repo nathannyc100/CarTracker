36dd351ee5e55196a7da7ff06555562e6b6acdfe
nathannyc100 <107283236+nathannyc100@users.noreply.github.com>
2024-01-20 11:53:01 +0800

    Initial Commit

---
+++ CarTracker/CarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTrackerApp.swift
+//  CarTracker
+//
+//  Created by Nathan Yu on 2024/1/20.
+//
+
+import SwiftUI
+
+@main
+struct CarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
